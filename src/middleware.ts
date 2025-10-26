aafca1aab769422d1685599bb1354cdc68cff487
Aymeric PINEAU <ay.pineau@gmail.com>
2025-10-27 00:18:08 +0100

    feat: update template

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 // src/middleware.ts
-export { intlayerMiddleware as middleware } from "next-intlayer/middleware";
+export { intlayerProxy as middleware } from "next-intlayer/middleware";
 
 export const config = {
   matcher: "/((?!api|static|.*\\..*|_next).*)",
